b9685f7c8767356e28843ff6d7d2268b84dec8c2
kurosaki-ui <p10p030@cis.ac.jp>
2020-07-20 21:45:37 +0900

    PinBall：Lesson5課題　初めてのコミット

---
+++ Assets/BallController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+	private float visiblePosZ = -6.5f;
+	private GameObject gameoverText;
+	private GameObject scoreText;
+	private int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		this.gameoverText = GameObject.Find("GameOverText");
+		this.scoreText = GameObject.Find("ScoreText");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.transform.position.z < this.visiblePosZ){
+			this.gameoverText.GetComponent<Text> ().text = "Game Over";
+		}
+	}
+	 void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.tag == "SmallStarTag")
+		{
+			this.score+=10;
+		}
+		else if(other.gameObject.tag == "LargeStarTag")
+		{
+			this.score+=20;
+		}
+		else if(other.gameObject.tag == "SmallCloudTag" || other.gameObject.tag == "LaregeCloudTag")
+		{
+			this.score+=30;
+		}
+		this.scoreText.GetComponent<Text>().text = "Score:" + score.ToString();
+	}
+}
